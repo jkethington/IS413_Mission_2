afc37212b9fa4c38d2a5c1d4b5ae0a60cb89fb49
jkethington <jkethington@gmail.com>
2026-01-14 17:39:43 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class DiceRoller
+{
+    public int[] RollDice(int numberOfRolls)
+    {
+        Random rand = new Random();
+        int[] results = new int[13]; // index = dice sum
+
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            int die1 = rand.Next(1, 7);
+            int die2 = rand.Next(1, 7);
+            int sum = die1 + die2;
+
+            results[sum]++;
+        }
+
+        
+        return results;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of times to roll: ");
+        int rolls = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Rolling the dice {rolls} times...\n");
+
+        DiceRoller roller = new DiceRoller();
+        int[] results = roller.RollDice(rolls);
+
+
+
+        System.Console.WriteLine("Distribution of rolls:");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            double percent = (double)results[i] / rolls * 100;
+
+            Console.Write($"{i}: ");
+
+            int stars = (int)percent;
+
+            for (int j = 0; j < stars; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
